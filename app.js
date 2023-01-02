56b83972a3dc98770edcd3f6e8dd97d77cc5c6c5
zihadul10101 <zihadul10101@gmail.com>
2023-01-02 21:24:36 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// loader start
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+            "body").style.visibility = "hidden";
+        document.querySelector(
+            "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+            "#loader").style.display = "none";
+        document.querySelector(
+            "body").style.visibility = "visible";
+    }
+};
+// loader end
+// navbar start
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  // navbar end
+
